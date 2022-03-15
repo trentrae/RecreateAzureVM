523840c43766eac4b14c3f792d2b43c92142ed2d
trentrae <39933317+trentrae@users.noreply.github.com>
2022-03-15 11:04:49 -0500

    Create recreatevm.ps1

---
+++ recreatevm.ps1
@@ -0,0 +1,62 @@
+# Set variables
+    $resourceGroup = "gov-test-avs_group"
+    $vmName = "gov-test-avs2"
+    #$newAvailSetName = "myAvailabilitySet"
+
+# Get the details of the VM to be moved to the Availability Set
+    $originalVM = Get-AzVM `
+       -ResourceGroupName $resourceGroup `
+       -Name $vmName
+
+# Stop the VM to take snapshot
+Stop-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+
+# Remove the original VM
+    Remove-AzVM -ResourceGroupName $resourceGroup -Name $vmName
+
+# Create the basic configuration for the replacement VM.
+    $newVM = New-AzVMConfig `
+       -VMName $originalVM.Name `
+       -VMSize $originalVM.HardwareProfile.VmSize `
+       #-AvailabilitySetId $availSet.Id
+ 
+# For a Linux VM, change the last parameter from -Windows to -Linux
+    Set-AzVMOSDisk `
+       -VM $newVM -CreateOption Attach `
+       -ManagedDiskId $originalVM.StorageProfile.OsDisk.ManagedDisk.Id `
+       -Name $originalVM.StorageProfile.OsDisk.Name `
+       -Windows
+
+# Add Data Disks
+    foreach ($disk in $originalVM.StorageProfile.DataDisks) { 
+    Add-AzVMDataDisk -VM $newVM `
+       -Name $disk.Name `
+       -ManagedDiskId $disk.ManagedDisk.Id `
+       -Caching $disk.Caching `
+       -Lun $disk.Lun `
+       -DiskSizeInGB $disk.DiskSizeGB `
+       -CreateOption Attach
+    }
+    
+# Add NIC(s) and keep the same NIC as primary; keep the Private IP too, if it exists.
+    foreach ($nic in $originalVM.NetworkProfile.NetworkInterfaces) {	
+    if ($nic.Primary -eq "True")
+    {
+            Add-AzVMNetworkInterface `
+               -VM $newVM `
+               -Id $nic.Id -Primary
+               }
+           else
+               {
+                 Add-AzVMNetworkInterface `
+                -VM $newVM `
+                 -Id $nic.Id 
+                }
+      }
+
+# Recreate the VM
+    New-AzVM `
+       -ResourceGroupName $resourceGroup `
+       -Location $originalVM.Location `
+       -VM $newVM `
+       -DisableBginfoExtension
